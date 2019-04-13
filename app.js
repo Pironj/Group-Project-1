e90d8e93c17f14e83a80d5e15bb4c8fd2591da83
Julian Piron <pironj27@gmail.com>
2019-04-13 11:05:34 -0700

    updated google maps location

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var artist = "punk";
 var genre = "";
 var size = 10;
 var zip = "";
-var city = "seattle";
+var city = "";
 
 var queryURL = "https://app.ticketmaster.com/discovery/v2/events?keyword=" + artist + "&city=" + city + "&postalCode=" + zip + "&size=" + size + "&apikey=" + key;
 
@@ -12,7 +12,7 @@ $.ajax({
   url: queryURL,
   method: "GET"
 }).then(function (response) {
-  // console.log(response);
+  console.log(response);
 
   for (var i = 0; i < response._embedded.events.length; i++) {
     console.log(response._embedded.events.length);
@@ -26,10 +26,28 @@ $.ajax({
     var time = response._embedded.events[i].dates.start.localTime;
     var date = response._embedded.events[i].dates.start.localDate;
     var venue = response._embedded.events[i]._embedded.venues[0].name;
+    var address = response._embedded.events[i]._embedded.venues[0].address.line1;
+    city = response._embedded.events[i]._embedded.venues[0].city.name;
+    var state = response._embedded.events[i]._embedded.venues[0].state.stateCode;
+    zip = response._embedded.events[i]._embedded.venues[0].postalCode;
     var buyTicket = response._embedded.events[i].url;
+
+    var location = $('<div>');
+    location.append(address);
+    var locCityState = $('<div>');
+    locCityState.append(city + ', ' + state);
+    location.append(locCityState);
+    var locZip = $('<div>');
+    locZip.append(zip);
+    location.append(locZip);
+    var directions = $('<a>Directions</a>');
+    directions.attr('href', "https://www.google.com/maps/place/" + address);
+    location.append(directions);
+
 
     var newRow = $('<tr>').append(
       $('<td>').text(artist),
+      $('<td>').append(location),
       $('<td>').text(venue),
       $('<td>').text(date),
       $('<td>').text(time),
